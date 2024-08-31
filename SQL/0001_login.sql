1ff7134457ae12e4b6ca6dcaa3cea47a1e37ef01
DEViana95 <deviana9595@gmail.com>
2024-08-30 22:57:24 -0300

    Att sql

--- SQL/0001_login.sql
+++ SQL/0001_login.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(
 	id SERIAL PRIMARY KEY,
 	login varchar(30) UNIQUE,
@@ -13,8 +14,30 @@ INSERT INTO users VALUES
     (DEFAULT, 'frederico.hartmann', '112233', 1)
 ;
 
-SELECT 
-    * 
-FROM users
+DROP TABLE IF EXISTS incident_types;
+CREATE TABLE IF NOT EXISTS incident_types(
+	id SERIAL PRIMARY KEY,
+	title TEXT NOT NULL,
+	active BOOLEAN NOT NULL
+);
+
+INSERT INTO incident_types VALUES
+    (DEFAULT, 'Inundações', false),
+    (DEFAULT, 'Deslizamentos de Terra' , false),
+    (DEFAULT, 'Tempestades', false),
+    (DEFAULT, 'Tornados', false),
+    (DEFAULT, 'Incêndios Florestais', false),
+    (DEFAULT, 'Cheias', false)
 ;
 
+DROP TABLE IF EXISTS parameters;
+CREATE TABLE IF NOT EXISTS parameters(
+	id SERIAL PRIMARY KEY,
+	coordinate_one DECIMAL NOT NULL,
+	coordinate_two DECIMAL NOT NULL,
+	coordinate_three DECIMAL NOT NULL,
+	coordinate_four DECIMAL NOT NULL,
+	cellphone TEXT NOT NULL,
+	administrator_id BIGINT NOT NULL,
+	CONSTRAINT fk_nome FOREIGN KEY (administrator_id) REFERENCES users(id)
+);
